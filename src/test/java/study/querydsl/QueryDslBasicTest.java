9e57239a9095dd765753ac5c822365329a498921
ndy2 <emrdbs12@gmail.com>
2022-05-19 02:12:17 +0900

    기본 문법 - 1,2
    시작 - JPQL vs Querydsl
    기본 Q-Type 활용

---
+++ src/test/java/study/querydsl/QueryDslBasicTest.java
@@ -0,0 +1,85 @@
+package study.querydsl;
+
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.entity.Member;
+import study.querydsl.entity.QMember;
+import study.querydsl.entity.Team;
+
+import javax.persistence.EntityManager;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static study.querydsl.entity.QMember.member;
+
+@SpringBootTest
+@Transactional
+class QueryDslBasicTest {
+
+    @Autowired
+    EntityManager em;
+
+    JPAQueryFactory query;
+
+    @BeforeEach
+    void setUp() {
+        query = new JPAQueryFactory(em);
+
+        Team teamA = new Team("해바라기반");
+        Team teamB = new Team("장미반");
+
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("짱구", 5, teamA);
+        Member member2 = new Member("유리", 5, teamA);
+        Member member3 = new Member("치타", 5, teamB);
+        Member member4 = new Member("둘리", 5, teamB);
+
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+
+        em.flush();
+        em.clear();
+
+        System.out.println("=====================");
+    }
+
+    @Test
+    void startJpql() {
+        Member findMember = em.createQuery("select m from Member m where m.username = :username", Member.class)
+                .setParameter("username", "짱구")
+                .getSingleResult();
+        
+        assertThat(findMember.getUsername()).isEqualTo("짱구");
+    }
+
+    @Test
+    void startQueryDslV1() {
+        QMember m = new QMember("m");
+
+        Member findMember = query
+                .select(m)
+                .from(m)
+                .where(m.username.eq("짱구"))
+                .fetchOne();
+
+        assertThat(findMember.getUsername()).isEqualTo("짱구");
+    }
+
+    @Test
+    void startQueryDslV2() {
+        Member findMember = query
+                .selectFrom(member)
+                .where(member.username.eq("짱구"))
+                .fetchOne();
+
+        assertThat(findMember.getUsername()).isEqualTo("짱구");
+    }
+}
