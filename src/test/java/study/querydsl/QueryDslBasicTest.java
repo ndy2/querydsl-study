25e7922ee4ae22c0aa3c67836a2bb6f0c50123cb
ndy2 <emrdbs12@gmail.com>
2022-05-19 16:49:10 +0900

    기본 문법 12 - case 문

--- src/test/java/study/querydsl/QueryDslBasicTest.java
+++ src/test/java/study/querydsl/QueryDslBasicTest.java
@@ -3,6 +3,7 @@ package study.querydsl;
 
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.Tuple;
+import com.querydsl.core.types.dsl.CaseBuilder;
 import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.junit.jupiter.api.BeforeEach;
@@ -376,4 +377,28 @@ class QueryDslBasicTest {
                             .from(sub)));
         }
     }
+
+    @Test
+    void case_문() {
+
+        List<String> result = query
+                .select(member.age.when(10).then("열살")
+                        .when(20).then("스무살")
+                        .otherwise("기타")).from(member)
+                .fetch();
+    }
+
+    @Test
+    void case_builder() {
+        List<String> result = query
+                .select(
+                        new CaseBuilder()
+                                .when(member.age.between(0, 20)).then("0~20살")
+                                .when(member.age.between(21, 30)).then("21~30살")
+                                .otherwise("기타"))
+                .from(member)
+                .fetch();
+    }
+
+
 }
