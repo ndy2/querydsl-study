b67d5878ca3c382e1d4192661dc990ff6c600b82
ndy2 <emrdbs12@gmail.com>
2022-05-19 23:54:43 +0900

    실무 활용 2. DTO 조회, 동적 쿼리 - Builder 사용

--- src/main/java/study/querydsl/repository/MemberJpaRepository.java
+++ src/main/java/study/querydsl/repository/MemberJpaRepository.java
@@ -1,15 +1,21 @@
 package study.querydsl.repository;
 
+import com.querydsl.core.BooleanBuilder;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+import study.querydsl.dto.MemberSearchCondition;
+import study.querydsl.dto.MemberTeamDto;
+import study.querydsl.dto.QMemberTeamDto;
 import study.querydsl.entity.Member;
 
 import javax.persistence.EntityManager;
 import java.util.List;
 import java.util.Optional;
 
+import static org.springframework.util.StringUtils.hasText;
 import static study.querydsl.entity.QMember.member;
+import static study.querydsl.entity.QTeam.team;
 
 @Repository
 @RequiredArgsConstructor
@@ -18,36 +24,69 @@ public class MemberJpaRepository {
     private final EntityManager em;
     private final JPAQueryFactory query;
 
-    public void save(Member member){
+    public void save(Member member) {
         em.persist(member);
     }
 
-    public Optional<Member> findById(Long id){
+    public Optional<Member> findById(Long id) {
         Member findMember = em.find(Member.class, id);
         return Optional.ofNullable(findMember);
     }
 
-    public List<Member> findAll(){
+    public List<Member> findAll() {
         return em.createQuery("select m from Member m", Member.class)
                 .getResultList();
     }
 
-    public List<Member> findAll_queryDsl(){
+    public List<Member> findAll_queryDsl() {
         return query
                 .selectFrom(member)
                 .fetch();
     }
 
-    public List<Member> findByUsername(String username){
+    public List<Member> findByUsername(String username) {
         return em.createQuery("select m from Member m where m.username = :username", Member.class)
                 .setParameter("username", username)
                 .getResultList();
     }
 
-    public List<Member> findByUsername_queryDsl(String username){
+    public List<Member> findByUsername_queryDsl(String username) {
         return query
                 .selectFrom(member)
                 .where(member.username.eq(username))
                 .fetch();
     }
+
+    public List<MemberTeamDto> searchByBuilder(MemberSearchCondition condition) {
+
+        BooleanBuilder builder = new BooleanBuilder();
+        if (hasText(condition.getUsername())) {
+            builder.and(member.username.eq(condition.getUsername()));
+        }
+        if (hasText(condition.getTeamName())) {
+            builder.and(team.name.eq(condition.getTeamName()));
+        }
+
+        if (condition.getAgeGoe() != null) {
+            builder.and(member.age.goe(condition.getAgeGoe()));
+        }
+
+        if (condition.getAgeLoe() != null) {
+            builder.and(member.age.loe(condition.getAgeLoe()));
+        }
+
+        return query
+                .select(new QMemberTeamDto(
+                        member.id.as("memberId"),
+                        member.username,
+                        member.age,
+                        team.id.as("teamId"),
+                        team.name.as("teamName")
+                ))
+                .from(member)
+                .leftJoin(member.team, team)
+                .where(builder)
+                .fetch();
+    }
+
 }
